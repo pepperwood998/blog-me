b8ddf457b3601ead9637060101a86cf94e7a9f99
zuratama <tuandt66742@gmail.com>
2020-08-05 21:19:13 +0700

    Prevent useEffect mem-leak and race condition

--- components/hook/useFetchListArticle.js
+++ components/hook/useFetchListArticle.js
@@ -10,17 +10,25 @@ export default function useFetchListArticle(offset = 0, limit = 20) {
   const [error, setError] = useState("");
 
   useEffect(() => {
+    let isReady = true;
+
     setLoading(true);
 
     (async () => {
       try {
         const response = await fetchListArticle(offset, limit);
-        setData({ ...response.data });
+        if (isReady) {
+          setData({ ...response.data });
+        }
       } catch (err) {
         setError("Failed to get articles");
       } finally {
         setLoading(false);
       }
+
+      return () => {
+        isReady = false;
+      };
     })();
   }, [offset, limit]);
 
